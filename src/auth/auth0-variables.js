e083fd7f23555ebb65792c2800acd27791dc1564
Julian Lau <julian_lau@hotmail.com>
2021-02-08 13:40:36 +0800

    rps2.0

--- src/auth/auth0-variables.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-    domain: "dev-hzl46juy.auth0.com",
-    roleUrl: "http://rpsweb2.0/role",
-    clientId: "jhPOuUr3YvgCbBhPtXZt3TLzK2D9A5JG",
-    callbackUrl: "http://localhost:8080"
-};
