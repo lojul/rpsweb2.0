4807df2bd832931e585bc2ec99d60924b74960d1
Julian Lau <julian_lau@hotmail.com>
2021-02-08 13:37:18 +0800

    rps2.0

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+    // eslint-disable-next-line react/jsx-no-undef
+    <Router>
+    <Auth0ProviderWithHistory>
+        <App />
+    </Auth0ProviderWithHistory>
+    </Router>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
